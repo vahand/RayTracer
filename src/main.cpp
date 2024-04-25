b36e1a5e1b25a7d9dc8dea967a7013662875e51f
vahand <vahan.ducher@epitech.eu>
2024-04-25 10:12:07 +0200

    [Edit] config file to display shapes properly

--- src/main.cpp
+++ src/main.cpp
@@ -18,25 +18,12 @@ int main()
     RayTracer::Parser parser(core, "./configs/subject_config");
     parser.printConfig();
 
-    // Math::Point3D lightPosition(0, 10, 0);
-    // double lightIntensity = 1.0;
-    // Math::Vector3D lightColor(1, 1, 1);
-    // RayTracer::Light light(lightPosition, lightIntensity, lightColor);
+    Math::Point3D lightPosition(0, 10, 0);
+    double lightIntensity = 1.0;
+    Math::Vector3D lightColor(1, 1, 1);
+    RayTracer::Light light(lightPosition, lightIntensity, lightColor);
 
-    // Math::Point3D sphereCenter(5, 5, 30);
-    // double sphereRadius = 2.0;
-    // RayTracer::Color sphereColor(255, 0, 0);
-    // RayTracer::Sphere sphere(sphereCenter, sphereRadius, sphereColor);
+    core.addLight(light);
 
-
-    // RayTracer::Plane plane;
-    // RayTracer::Color planeColor(0, 255, 0);
-    // Math::Point3D planeOrigin(0, 15, 0);
-    // plane.setup(planeColor, planeOrigin, RayTracer::Plane::AXIS::Y);
-
-    // core.addShape(sphere);
-    // core.addShape(plane);
-    // core.addLight(light);
-
-    // core.run();
+    core.run();
 }
