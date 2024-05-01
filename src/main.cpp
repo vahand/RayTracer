2dab572e1e3e350c30c43097801d0494d9e91c24
Mathis <mathis.emaille@gmail.com>
2024-05-01 17:37:44 +0200

    [Feat] Add -g, --gui: to enable GUI feature (not working yet); Add -t, --threads [count] to set the numbers of threads used to render
    Signed-off-by: Mathis <mathis.emaille@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 
 #include "../includes/Workers.hpp"
 
+#include "../includes/SFMLDisplay.hpp"
+
 static bool hasOption(int ac, char **av, const std::string &option)
 {
     for (int i = 0; i < ac; i++) {
@@ -35,22 +37,36 @@ static void *getOptionValue(int ac, char **av, const std::string &option, const 
 
 int main(int ac, char **av)
 {
+    std::unique_ptr<Graphics::AGraphicals> display;
+
     if (hasOption(ac, av, "-h") || hasOption(ac, av, "--help")) {
         std::cerr << "Usage: ./raytracer [options] > [image_path]" << std::endl;
         std::cerr << "\nOptions:" << std::endl;
         std::cerr << "\t-h, --help:\t Display this help message" << std::endl;
         std::cerr << "\t-t, --threads:\t Set the numbers of threads used for rendering" << std::endl;
+        std::cerr << "\t-g, --gui:\t Enable the GUI" << std::endl;
         return 0;
     }
+
     bool threadsOption = hasOption(ac, av, "-t") || hasOption(ac, av, "--threads");
     int threadsCount = 1;
     if (threadsOption) {
         threadsCount = std::stoi(static_cast<const char *>(getOptionValue(ac, av, "--threads", "-t")));
     }
 
-    RayTracer::Core core(400, 400);
-    Workers workers(threadsCount, 400, 400);
-    core.loadLibrairies();
+    bool guiOption = hasOption(ac, av, "-g") || hasOption(ac, av, "--gui");
+
+    // RayTracer::Core core(400, 400);
+    // Workers workers(threadsCount, 400, 400);
+    std::shared_ptr<RayTracer::Core> core = std::make_shared<RayTracer::Core>(400, 400);
+    std::shared_ptr<Workers> workers = std::make_shared<Workers>(threadsCount, 400, 400);
+    core->loadLibrairies();
+
+    if (guiOption) {
+        display = std::make_unique<Graphics::SFML::SFMLDisplay>();
+        display->setup(core, workers);
+        display->createWindow(1280, 720, "RayTracer");
+    }
 
     // RayTracer::Parser parser(core, "./configs/subject_config");
     // parser.printConfig();
@@ -82,22 +98,33 @@ int main(int ac, char **av)
     RayTracer::Sphere sphere_light2(sphere_light2_pos, sphereRadius, material_right);
     RayTracer::Sphere sphere_light3(sphere_light3_pos, sphereRadius, material_right);
 
-    core.addShape(sphere_light1);
-    core.addShape(sphere_light2);
-    core.addShape(sphere_light3);
-    core.addShape(plane);
+    core->addShape(sphere_light1);
+    core->addShape(sphere_light2);
+    core->addShape(sphere_light3);
+    core->addShape(plane);
 
-    // core.addShape(sphere_ground);
-    core.addShape(sphere_center);
-    core.addShape(sphere_left);
+    // core->addShape(sphere_ground);
+    core->addShape(sphere_center);
+    core->addShape(sphere_left);
 
     std::cerr << "Rendering settings: " << std::endl;
-    std::cerr << " - Width: " << core._camera._image_width << std::endl;
-    std::cerr << " - Height: " << core._camera._image_height << std::endl;
-    std::cerr << " - Quality (samples): " << core._camera._samples << std::endl;
-    std::cerr << " - Max depth: " << core._maxDepth << std::endl;
-    std::cerr << " - Threads: " << workers.getThreadsCount() << std::endl;
+    std::cerr << " - Width: " << core->_camera._image_width << std::endl;
+    std::cerr << " - Height: " << core->_camera._image_height << std::endl;
+    std::cerr << " - Quality (samples): " << core->_camera._samples << std::endl;
+    std::cerr << " - Max depth: " << core->_maxDepth << std::endl;
+    std::cerr << " - Threads: " << workers->getThreadsCount() << std::endl;
 
-    workers.initialize(core);
-    workers.render(core);
+    if (guiOption) {
+        std::cerr << "Rendering with GUI" << std::endl;
+        while (display->isWindowOpen()) {
+            display->renderAll();
+        }
+    } else {
+        workers->initialize(*core);
+        // workers->render(*core);
+        workers->setRendering(true);
+        workers->beginRender();
+        while (workers->isRendering())
+            workers->renderUpdate(*core);
+    }
 }
