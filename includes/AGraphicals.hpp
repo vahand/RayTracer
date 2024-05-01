22b56f066878116e7191ac1bac7f268eba6d15ff
Mathis <mathis.emaille@gmail.com>
2024-05-01 17:36:39 +0200

    [Add] Interface and Abstract class for displayer module
    Signed-off-by: Mathis <mathis.emaille@gmail.com>

---
+++ includes/AGraphicals.hpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2024
+** RAYTRACER
+** File description:
+** AGraphicals
+*/
+
+#ifndef AGRAPHICALS_HPP_
+    #define AGRAPHICALS_HPP_
+
+    #include "IGraphicals.hpp"
+
+namespace Graphics {
+    class AGraphicals : public IGraphicals {
+        public:
+            AGraphicals() = default;
+            ~AGraphicals() = default;
+
+            virtual void setup(std::shared_ptr<RayTracer::Core> core, std::shared_ptr<Workers> workers) = 0;
+            virtual void createWindow(int width, int height, const std::string& title) {};
+            virtual void closeWindow() {};
+            virtual void clearWindow() {};
+            virtual void displayWindow() {};
+            virtual void getEvents() {};
+            virtual void renderAll() {};
+            virtual void updateRenderedImage(std::unordered_map<int, std::vector<RayTracer::Color>> image) {};
+
+            virtual bool isWindowOpen() { return false; };
+
+        protected:
+        private:
+    };
+}
+
+
+#endif /* !AGRAPHICALS_HPP_ */
---
+++ includes/IGraphicals.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2024
+** RAYTRACER
+** File description:
+** IGraphicals
+*/
+
+#ifndef IGRAPHICALS_HPP_
+    #define IGRAPHICALS_HPP_
+
+    #include <string>
+    #include "Workers.hpp"
+    #include "Core.hpp"
+
+class IGraphicals {
+    public:
+        IGraphicals() = default;
+        ~IGraphicals() = default;
+
+        virtual void setup(std::shared_ptr<RayTracer::Core> core, std::shared_ptr<Workers> workers) = 0;
+        virtual void createWindow(int width, int height, const std::string& title) = 0;
+        virtual void closeWindow() = 0;
+        virtual bool isWindowOpen() = 0;
+        virtual void clearWindow() = 0;
+        virtual void displayWindow() = 0;
+        virtual void getEvents() = 0;
+        virtual void renderAll() = 0;
+
+        virtual void updateRenderedImage(std::unordered_map<int, std::vector<RayTracer::Color>> image) = 0;
+
+    protected:
+    private:
+};
+
+#endif /* !IGRAPHICALS_HPP_ */
