3c90c4604b269b528a0ec3ec0caab5214ed28c5c
vahand <vahan.ducher@epitech.eu>
2024-05-02 13:25:17 +0200

    [Feat] add hit function for Cube shape / not functional

--- primitives/cube/src/Cube.hpp
+++ primitives/cube/src/Cube.hpp
@@ -12,28 +12,99 @@
     #include "../../../includes/HitData.hpp"
     #include "../../../includes/Range.hpp"
     #include "../../../includes/Ray.hpp"
-    #include "../../../includes/AMaterial.hpp"
-    #include "../../../includes/Color.hpp"
-    #include "../../../includes/ShapeConfig.hpp"
     #include <unordered_map>
 
 namespace RayTracer {
+    class CubeFace {
+        public:
+            CubeFace() = default;
+            CubeFace(Math::Point3D topLeftCorner, Math::Point3D topRightCorner,
+            Math::Point3D bottomLeftCorner, Math::Point3D bottomRightCorner,
+            Math::Vector3D normal)
+            : _topLeftCorner(topLeftCorner), _topRightCorner(topRightCorner),
+            _bottomLeftCorner(bottomLeftCorner), _bottomRightCorner(bottomRightCorner),
+            _normal(normal) {
+                _min_X = topLeftCorner.X;
+                _max_X = topRightCorner.X;
+                _min_Y = topLeftCorner.Y;
+                _max_Y = bottomLeftCorner.Y;
+                _min_Z = topLeftCorner.Z;
+                _max_Z = bottomLeftCorner.Z;
+            }
+
+            ~CubeFace() = default;
+
+            double getDConstante() const
+            {
+                double d = _normal.X * _topLeftCorner.X + _normal.Y * _topLeftCorner.Y + _normal.Z * _topLeftCorner.Z;
+                return d;
+            }
+
+            Math::Point3D _topLeftCorner;
+            Math::Point3D _topRightCorner;
+            Math::Point3D _bottomLeftCorner;
+            Math::Point3D _bottomRightCorner;
+            double _min_X;
+            double _max_X;
+            double _min_Y;
+            double _max_Y;
+            double _min_Z;
+            double _max_Z;
+
+            Math::Vector3D _normal;
+
+            double d = getDConstante();
+
+            bool hit(const RayTracer::Ray& ray, RayTracer::Range ray_range, HitData& data) const
+            {
+                    double d = getDConstante();
+                    double t = (d - ray.origin().DotProduct(getNormalVector())) / getNormalVector().DotProduct(ray.direction());
+                    if (t < ray_range.min || t > ray_range.max)
+                        return false;
+                    Math::Point3D ray_point = ray.at(t);
+                    if (ray_point.X < _min_X || ray_point.X > _max_X)
+                        return false;
+                    if (ray_point.Y < _min_Y || ray_point.Y > _max_Y)
+                        return false;
+                    if (ray_point.Z < _min_Z || ray_point.Z > _max_Z)
+                        return false;
+                    data.tValue = t;
+                    data.point = ray.at(data.tValue);
+                    data.normal = getNormalVector();
+                    return true;
+            }
+
+            Math::Vector3D getNormalVector() const
+            {
+                return _normal;
+            }
+    };
     class Cube : public AShape {
         public:
-            class CubeFace {
-                public:
-                    CubeFace(Math::Point3D highLeftCorner, Math::Point3D highRightCorner, Math::Point3D lowLeftCorner, Math::Point3D lowRightCorner);
-                    ~CubeFace();
-            };
+
             Cube() = default;
             Cube(const Math::Point3D origin, double xDim, double yDim, double zDim, std::shared_ptr<RayTracer::Material::IMaterial> material)
                 : _material(material), _origin(origin), _xDim(xDim), _yDim(yDim), _zDim(zDim)
             {
+                initVertexes();
+                initFaces();
+            }
 
+            void setup(const RayTracer::ShapeConfig& config)
+            {
             }
 
             bool hit(const RayTracer::Ray& ray, RayTracer::Range ray_range, HitData& data) const override
             {
+                // call hit for each face
+                // update HitData with the closest face's data
+                for (auto& face : _faces) {
+                    if (face.second.hit(ray, ray_range, data)) {
+                        data.determineFace(ray, data.normal);
+                        data.material = _material;
+                        return true;
+                    }
+                }
                 return false;
             }
 
@@ -50,12 +121,29 @@ namespace RayTracer {
             double _zDim;
             std::shared_ptr<RayTracer::Material::IMaterial> _material;
             std::unordered_map<std::string, Math::Point3D> _vertexes;
+            std::unordered_map<std::string, CubeFace> _faces;
 
-            double xO() { return _origin.x(); }
-            double yO() { return _origin.y(); }
-            double zO() { return _origin.z(); }
+            double xO() const { return _origin.x(); }
+            double yO() const { return _origin.y(); }
+            double zO() const { return _origin.z(); }
 
-            void init_vertexes()
+            Math::Point3D A() { return _vertexes["A"]; }
+            Math::Point3D B() { return _vertexes["B"]; }
+            Math::Point3D C() { return _vertexes["C"]; }
+            Math::Point3D D() { return _vertexes["D"]; }
+            Math::Point3D E() { return _vertexes["E"]; }
+            Math::Point3D F() { return _vertexes["F"]; }
+            Math::Point3D G() { return _vertexes["G"]; }
+            Math::Point3D H() { return _vertexes["H"]; }
+
+            CubeFace frontFace() { return _faces["frontFace"]; }
+            CubeFace backFace() { return _faces["backFace"]; }
+            CubeFace topFace() { return _faces["topFace"]; }
+            CubeFace bottomFace() { return _faces["bottomFace"]; }
+            CubeFace leftFace() { return _faces["leftFace"]; }
+            CubeFace rightFace() { return _faces["rightFace"]; }
+
+            void initVertexes()
             {
                 _vertexes["A"] = Math::Point3D(xO() - _xDim / 2.0, yO() - _yDim / 2.0, zO() - _zDim / 2.0);
                 _vertexes["B"] = Math::Point3D(xO() + _xDim / 2.0, yO() - _yDim / 2.0, zO() - _zDim / 2.0);
@@ -65,7 +153,15 @@ namespace RayTracer {
                 _vertexes["F"] = Math::Point3D(xO() + _xDim / 2.0, yO() - _yDim / 2.0, zO() + _zDim / 2.0);
                 _vertexes["G"] = Math::Point3D(xO() - _xDim / 2.0, yO() + _yDim / 2.0, zO() + _zDim / 2.0);
                 _vertexes["H"] = Math::Point3D(xO() + _xDim / 2.0, yO() + _yDim / 2.0, zO() + _zDim / 2.0);
+            }
 
+            void initFaces() {
+                _faces["frontFace"] = CubeFace(A(), B(), C(), D(), Math::Vector3D(0, 0, 1));
+                _faces["backFace"] = CubeFace(E(), F(), G(), H(), Math::Vector3D(0, 0, 1));
+                _faces["topFace"] = CubeFace(E(), F(), A(), B(), Math::Vector3D(0, 1, 0));
+                _faces["bottomFace"] = CubeFace(G(), H(), C(), D(), Math::Vector3D(0, 1, 0));
+                _faces["leftFace"] = CubeFace(E(), A(), G(), C(), Math::Vector3D(1, 0, 0));
+                _faces["rightFace"] = CubeFace(B(), D(), F(), H(), Math::Vector3D(1, 0, 0));
             }
     };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -24,45 +24,50 @@ int main()
         Workers workers(8, 400, 400);
         core.loadLibrairies();
 
-        RayTracer::Parser parser(core, "./configs/mathis_config3");
-        parser.printConfig();
+        // RayTracer::Parser parser(core, "./configs/mathis_config3");
+        // parser.printConfig();
 
         // RayTracer::Parser parser(core, "./configs/subject_config");
 
-        // double sphereRadius = 2.0;
+        double sphereRadius = 2.0;
 
-        // auto material_ground = std::make_shared<RayTracer::Material::Lambertian>(RayTracer::Color(255, 255, 0));
-        // auto material_center = std::make_shared<RayTracer::Material::Lambertian>(RayTracer::Color(0, 255, 255));
-        // auto material_left   = std::make_shared<RayTracer::Material::Metal>(RayTracer::Color(255, 0, 0), 0.0);
-        // auto material_right  = std::make_shared<RayTracer::Material::LightDiffuse>(RayTracer::Color(255, 255, 255));
+        auto material_ground = std::make_shared<RayTracer::Material::Lambertian>(RayTracer::Color(255, 255, 0));
+        auto material_center = std::make_shared<RayTracer::Material::Lambertian>(RayTracer::Color(0, 255, 255));
+        auto material_cube = std::make_shared<RayTracer::Material::Lambertian>(RayTracer::Color(0, 0, 255));
+        auto material_left   = std::make_shared<RayTracer::Material::Metal>(RayTracer::Color(255, 0, 0), 0.0);
+        auto material_right  = std::make_shared<RayTracer::Material::LightDiffuse>(RayTracer::Color(255, 255, 255));
 
-        // Math::Point3D sphere_left_pos(2.5, 8, 20);
-        // RayTracer::Sphere sphere_left(sphere_left_pos, sphereRadius, material_left);
+        Math::Point3D sphere_left_pos(2.5, 8, 20);
+        RayTracer::Sphere sphere_left(sphere_left_pos, sphereRadius, material_left);
 
-        // Math::Point3D sphere_center_pos(7.5, 8, 20);
-        // RayTracer::Sphere sphere_center(sphere_center_pos, sphereRadius, material_center);
+        Math::Point3D sphere_center_pos(7.5, 8, 20);
+        RayTracer::Sphere sphere_center(sphere_center_pos, sphereRadius, material_center);
 
-        // Math::Point3D sphere_ground_pos(7.5, 108, 0);
-        // RayTracer::Sphere sphere_ground(sphere_ground_pos, 100, material_ground);
+        Math::Point3D sphere_ground_pos(7.5, 108, 0);
+        RayTracer::Sphere sphere_ground(sphere_ground_pos, 100, material_ground);
 
-        // Math::Point3D plane_position(0, 9, 0);
-        // RayTracer::Plane plane(plane_position, RayTracer::ShapeConfig::AXIS::Y, material_ground);
+        Math::Point3D plane_position(0, 9, 0);
+        RayTracer::Plane plane(plane_position, RayTracer::ShapeConfig::AXIS::Y, material_ground);
 
-        // Math::Point3D sphere_light1_pos(12.5, 8, 10);
-        // Math::Point3D sphere_light2_pos(12.5, 8, 20);
-        // Math::Point3D sphere_light3_pos(7.5, 0, 20);
+        Math::Point3D sphere_light1_pos(12.5, 8, 10);
+        Math::Point3D sphere_light2_pos(12.5, 8, 20);
+        Math::Point3D sphere_light3_pos(7.5, 0, 20);
 
-        // RayTracer::Sphere sphere_light1(sphere_light1_pos, sphereRadius, material_right);
-        // RayTracer::Sphere sphere_light2(sphere_light2_pos, sphereRadius, material_right);
-        // RayTracer::Sphere sphere_light3(sphere_light3_pos, sphereRadius, material_right);
+        RayTracer::Sphere sphere_light1(sphere_light1_pos, sphereRadius, material_right);
+        RayTracer::Sphere sphere_light2(sphere_light2_pos, sphereRadius, material_right);
+        RayTracer::Sphere sphere_light3(sphere_light3_pos, sphereRadius, material_right);
 
-        // core.addShape(sphere_light1);
-        // core.addShape(sphere_light2);
-        // core.addShape(sphere_light3);
-        // core.addShape(plane);
+        Math::Point3D cube_position(7.5, 8, 15);
+        RayTracer::Cube cube(cube_position, 2, 2, 2, material_cube);
 
-        // core.addShape(sphere_center);
-        // core.addShape(sphere_left);
+        core.addShape(sphere_light1);
+        core.addShape(sphere_light2);
+        core.addShape(sphere_light3);
+        core.addShape(plane);
+        core.addShape(cube);
+
+        core.addShape(sphere_center);
+        core.addShape(sphere_left);
 
         std::cerr << "Rendering settings: " << std::endl;
         std::cerr << " - Width: " << core._camera._image_width << std::endl;
